b105e3113a60dde1800da01de1e5ced24fae3e05
klimanovakateryna <klimanovakateryna@gmail.com>
2025-05-11 16:06:11 -0400

    Refactor cost_match function and initialize matching cost matrix

--- kk8180h13.py
+++ kk8180h13.py
@@ -10,23 +10,37 @@
 
 # Once found, we need to print the cost, show where the match starts, print the character alignment
 
-#import sys
+import sys
+import string
+import numpy as np
 
 #target = sys.argv[1]
 #book = sys.stdin.read()
 
-import string
+target = "T e"
+book = "test"
+
+# 2D array to store the cost of matching the target string with the book
+dp = np.zeros((len(target) + 1, len(book) + 1), dtype=int)
+
+
+
 
 def cost_match(char1, char2):
     if char1 == char2:
         return 0
     elif char1 in string.printable and char2 in string.printable:
         return 3
-    elif char1 in string.printable and char2 not in string.printable:
+    elif (char1 in string.printable) != (char2 not in string.printable) :
         return 5
+    else: 
+        return 3 #both non-printable, but a match
+    
+
     
 # test
 
 print(cost_match('a', 'a'))
 print(cost_match('a', '/b'))
+print(cost_match('a', 'a')) 
         
